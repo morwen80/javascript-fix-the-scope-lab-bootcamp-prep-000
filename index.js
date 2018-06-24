5596005895faba48d37669290c1eedb9342c37d4
Valeria Ragonese <ribesnero80@gmail.com>
2018-06-24 13:31:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  // animal = cat was added. REturn animal was inside function
+  // animal = 'cat' was added. 
   animal = "cat";
   return animal
-  
 }
-
 
 
 function add2(n) {
